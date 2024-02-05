6266b5827a3dcd625cece11a1f7632da3e82ee27
solidoracle <solidoracle17@gmail.com>
2024-02-05 11:28:11 +0100

    testing simplified

--- test/foundry/BullaFactoring.t.sol
+++ test/foundry/BullaFactoring.t.sol
@@ -13,7 +13,7 @@ contract TestBullaFactoring is Test {
     address alice = address(0xA11c3);
     address bob = address(0xb0b);
 
-  function setUp() public {
+    function setUp() public {
         asset = new MockUSDC();
         bullaFactoring = new BullaFactoring(asset);
 
@@ -29,163 +29,113 @@ contract TestBullaFactoring is Test {
         vm.stopPrank();
     }
 
-    function testFactoringDepositWorkflow() private {
-        uint256 invoiceId = 1;
-        console.log("Invoice face value 100");
-        uint256 faceValue = 100;
-        uint256 depositAmount = 10000;
-        uint256 dueDate = block.timestamp + 30 days;
-
-        // Alice deposits 1k USDC
-        vm.startPrank(alice);
-        bullaFactoring.deposit(depositAmount, alice);
-        vm.stopPrank();
-        console.log("Alice deposits 1k USDC");
-
-        // Bob deposits 100 ether
-        vm.startPrank(bob);
-        bullaFactoring.deposit(depositAmount, bob);
-        vm.stopPrank();
-        console.log("Bob deposits 1k USDC");
-
-        // Log price per share before factoring
-        uint256 pricePerShareBefore = bullaFactoring.pricePerShare();
-        console.log("Price per share before factoring: ", pricePerShareBefore);
-
-        // Check NAV before factoring
-        uint256 navBefore = bullaFactoring.totalAssets();
-        console.log("NAV before factoring: ", navBefore);
-
-        // Bob factors an invoice
-        console.log("Bob factors a 100 USDC invoice");
-        vm.startPrank(bob);
-        bullaFactoring.fundInvoice(invoiceId, faceValue, dueDate);
-        vm.stopPrank();
-
-        // Check NAV after factoring
-        uint256 navAfterFactoring = bullaFactoring.totalAssets();
-        console.log("NAV after factoring: ", navAfterFactoring);
-        assertTrue(navAfterFactoring < navBefore, "NAV should decline after factoring");
-
-        // Log price per share after factoring
-        uint256 pricePerShareAfterFactoring = bullaFactoring.pricePerShare();
-        console.log("Price per share after factoring: ", pricePerShareAfterFactoring);
-
-        // Bob pays the invoice
-        console.log("Bob pays the invoice");
-        vm.startPrank(bob);
-        bullaFactoring.payInvoice(invoiceId);
-        vm.stopPrank();
-
-        // Check NAV after invoice is paid
-        uint256 navAfterPayment = bullaFactoring.totalAssets();
-        console.log("NAV after invoice is paid: ", navAfterPayment);
-        assertTrue(navAfterPayment > navAfterFactoring, "NAV should increase after invoice is paid");
-
-        // Log price per share after invoice payment
-        uint256 pricePerShareAfterPayment = bullaFactoring.pricePerShare();
-        console.log("Price per share after invoice payment: ", pricePerShareAfterPayment);
-
+    function calculateFundedAmount(uint256 faceValue, uint256 fundingPercentage) internal pure returns (uint256) {
+        return (faceValue * fundingPercentage) / 10000;
+    }
+    
+    function calculatePricePerShare(uint256 capitalAccount, uint256 sharesOutstanding, uint SCALING_FACTOR) public pure returns (uint256) {
+        if (sharesOutstanding == 0) return 0;
+        return (capitalAccount * SCALING_FACTOR) / sharesOutstanding;
     }
 
-    function testFactoringWorkflowWithRedemptions() public {
+    function testDepositAndRedemption() public {
+        uint256 dueDate = block.timestamp + 30 days;
+
         // Initial deposit of 2000 USDC
         uint256 initialDeposit = 2000;
-        uint256 dueDate = block.timestamp + 30 days;
         vm.startPrank(alice);
         bullaFactoring.deposit(initialDeposit, alice);
         vm.stopPrank();
-        console.log("Initial deposit of 2000 USDC by Alice");
-        console.log("totalSupply(): ", bullaFactoring.totalSupply());
+        
+        assertEq(bullaFactoring.totalSupply(), initialDeposit, "Initial total supply should equal initial deposit");
 
-        // Log price per share after initial deposit
         uint256 pricePerShare = bullaFactoring.pricePerShare();
-        console.log("Price per share after initial deposit: ", pricePerShare);
-        // log capital account
         uint capitalAccount = bullaFactoring.calculateCapitalAccount();
-        console.log("Capital account after initial deposit: ", capitalAccount);
-        console.log("totalAssets(): ", bullaFactoring.totalAssets());
-        console.log("totalDeposits(): ", bullaFactoring.totalDeposits());
-        console.log("totalWithdrawals(): ", bullaFactoring.totalWithdrawals());
+        uint sharePriceCheck = calculatePricePerShare(capitalAccount, bullaFactoring.totalSupply(), bullaFactoring.SCALING_FACTOR());
+        assertEq(pricePerShare, sharePriceCheck);
+        assertEq(capitalAccount, initialDeposit, "Initial capital account should equal initial deposit");
+        assertEq(bullaFactoring.totalAssets(), initialDeposit, "Initial capital account should equal initial deposit");
 
         // Bob funds an invoice for 100 USDC
         uint256 invoiceId1 = 1;
         vm.startPrank(bob);
-        bullaFactoring.fundInvoice(invoiceId1, 100, dueDate);
+        uint firstInvoiceAmount = 100;
+        uint firstFundedAmount = calculateFundedAmount(firstInvoiceAmount, bullaFactoring.fundingPercentage());
+        bullaFactoring.fundInvoice(invoiceId1, firstInvoiceAmount, dueDate);
         vm.stopPrank();
-        console.log("Alice funds an invoice for 100 USDC");
 
         // Partial redemption of 1000 USDC
         vm.startPrank(alice);
-        bullaFactoring.redeem(1000, alice, alice);
+        uint firstRedemption = 1000;
+        bullaFactoring.redeem(firstRedemption, alice, alice);
         vm.stopPrank();
-        console.log("Alice redeems 1000 USDC");
 
-        // Log price per share after partial redemption
         pricePerShare = bullaFactoring.pricePerShare();
-        console.log("Price per share after partial redemption: ", pricePerShare);
-        // log capital account
         capitalAccount = bullaFactoring.calculateCapitalAccount();
-        console.log("Capital account after partial redemption: ", capitalAccount);
-        console.log("totalSupply(): ", bullaFactoring.totalSupply());
-        console.log("totalAssets(): ", bullaFactoring.totalAssets());
-        console.log("totalDeposits(): ", bullaFactoring.totalDeposits());
-        console.log("totalWithdrawals(): ", bullaFactoring.totalWithdrawals());
+        sharePriceCheck = calculatePricePerShare(capitalAccount, bullaFactoring.totalSupply(), bullaFactoring.SCALING_FACTOR());
+        assertEq(pricePerShare, sharePriceCheck);
+        assertEq(capitalAccount, initialDeposit - firstRedemption, "Capital account should be the differentce between initial deposit and what has been redeemed so far");
+        assertEq(bullaFactoring.totalSupply(), initialDeposit - firstRedemption);
+        uint expectedTotalAssets = initialDeposit - firstRedemption - firstFundedAmount;
+        assertEq(bullaFactoring.totalAssets(), initialDeposit - firstRedemption - firstFundedAmount, "Total assets should reflect the amount of stables in the vault");
 
         // Bob funds a second invoice for 900 USDC
         uint256 invoiceId2 = 2;
         vm.startPrank(bob);
-        bullaFactoring.fundInvoice(invoiceId2, 900, dueDate);
+        uint secondInvoiceAmount = 900;
+        bullaFactoring.fundInvoice(invoiceId2, secondInvoiceAmount, dueDate);
+        uint secondFundedAmount = calculateFundedAmount(secondInvoiceAmount, bullaFactoring.fundingPercentage());
         vm.stopPrank();
-        console.log("Alice funds a second invoice for 900 USDC");
 
         // Partial redemption of 100 USDC
         vm.startPrank(alice);
-        bullaFactoring.redeem(100, alice, alice);
+        uint secondRedemption = 100;
+        bullaFactoring.redeem(secondRedemption, alice, alice);
         vm.stopPrank();
-        console.log("Alice redeems 100 USDC");
 
-        // Log price per share after second partial redemption
         pricePerShare = bullaFactoring.pricePerShare();
-        console.log("Price per share after second partial redemption: ", pricePerShare);
-        // log capital account
         capitalAccount = bullaFactoring.calculateCapitalAccount();
-        console.log("Capital account after second partial redemption: ", capitalAccount);
-        console.log("totalAssets(): ", bullaFactoring.totalAssets());
-        console.log("totalDeposits(): ", bullaFactoring.totalDeposits());
-        console.log("totalWithdrawals(): ", bullaFactoring.totalWithdrawals());
+        sharePriceCheck = calculatePricePerShare(capitalAccount, bullaFactoring.totalSupply(), bullaFactoring.SCALING_FACTOR());
+        assertEq(pricePerShare, sharePriceCheck);
+        assertEq(capitalAccount, initialDeposit - firstRedemption - secondRedemption, "Capital account should be the differentce between initial deposit and what has been redeemed so far");
+        assertEq(bullaFactoring.totalSupply(), initialDeposit - firstRedemption - secondRedemption);
+        expectedTotalAssets = initialDeposit - firstRedemption - secondRedemption - firstFundedAmount - secondFundedAmount;
+        assertEq(bullaFactoring.totalAssets(), expectedTotalAssets, "Total assets should reflect the amount of stables in the vault");
+    }
+
+    function testPricePerToken() public {
+        uint256 dueDate = block.timestamp + 30 days;
+
+        uint256 initialDeposit = 900;
+        vm.startPrank(alice);
+        bullaFactoring.deposit(initialDeposit, alice);
+        vm.stopPrank();
+
+        uint256 invoiceId1 = 1;
+        vm.startPrank(bob);
+        uint firstInvoiceAmount = 100;
+        bullaFactoring.fundInvoice(invoiceId1, firstInvoiceAmount, dueDate);
+        vm.stopPrank();
+
+        uint256 invoiceId2 = 2;
+        vm.startPrank(bob);
+        uint secondInvoiceAmount = 900;
+        bullaFactoring.fundInvoice(invoiceId2, secondInvoiceAmount, dueDate);
+        vm.stopPrank();
 
         // First invoice gets paid back for 100 USDC
         vm.startPrank(bob);
         bullaFactoring.payInvoice(invoiceId1);
         vm.stopPrank();
-        console.log("First invoice gets paid back for 100 USDC");
-
-        // Log price per share after first invoice payment
-        pricePerShare = bullaFactoring.pricePerShare();
-        console.log("Price per share after first invoice payment: ", pricePerShare);
-        // log capital account
-        capitalAccount = bullaFactoring.calculateCapitalAccount();
-        console.log("Capital account after first invoice payment: ", capitalAccount);
-        console.log("totalAssets(): ", bullaFactoring.totalAssets());
-        console.log("totalDeposits(): ", bullaFactoring.totalDeposits());
-        console.log("totalWithdrawals(): ", bullaFactoring.totalWithdrawals());
 
         // Second invoice gets paid back for 900 USDC
         vm.startPrank(bob);
         bullaFactoring.payInvoice(invoiceId2);
         vm.stopPrank();
-        console.log("Second invoice gets paid back for 900 USDC");
 
-        // Log price per share after second invoice payment
-        pricePerShare = bullaFactoring.pricePerShare();
-        console.log("Price per share after second invoice payment: ", pricePerShare);
-        // log capital account
-        capitalAccount = bullaFactoring.calculateCapitalAccount();
-        console.log("Capital account after second invoice payment: ", capitalAccount);
-        console.log("totalAssets(): ", bullaFactoring.totalAssets());
-        console.log("totalDeposits(): ", bullaFactoring.totalDeposits());
-        console.log("totalWithdrawals(): ", bullaFactoring.totalWithdrawals());
+        uint pricePerShare = bullaFactoring.pricePerShare();
+        uint capitalAccount = bullaFactoring.calculateCapitalAccount();
+        uint sharePriceCheck = calculatePricePerShare(capitalAccount, bullaFactoring.totalSupply(), bullaFactoring.SCALING_FACTOR());
+        assertEq(pricePerShare, sharePriceCheck);
     }
-
 }
